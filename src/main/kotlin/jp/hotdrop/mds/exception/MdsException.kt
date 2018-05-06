47a898afced2ce3d193556d810777346ddbefcff
kenji <gunsecondstage0106@gmail.com>
2018-05-06 13:13:03 +0900

    Delete because not used for now.

--- src/main/kotlin/jp/hotdrop/mds/exception/MdsException.kt
+++
@@ -1,7 +0,0 @@
-package jp.hotdrop.mds.exception
-
-class MdsException constructor(
-        val status: Int,
-        override val message: String,
-        override val cause: Throwable? = null
-): RuntimeException(message, cause)
