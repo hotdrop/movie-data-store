c7a3a9579c48b91bd182020340945ff656bd60a9
kenji <gunsecondstage0106@gmail.com>
2018-05-06 13:12:15 +0900

    Modified uri of API with reference to various sites.

--- src/main/kotlin/jp/hotdrop/mds/api/MovieController.kt
+++ src/main/kotlin/jp/hotdrop/mds/api/MovieController.kt
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/movies")
+@RequestMapping("/api/v1/movies")
 class MovieController @Autowired constructor(
         val service: MovieService
 ) {
